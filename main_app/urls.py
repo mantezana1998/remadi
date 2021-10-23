7b6a87c720bda0f4ab04a6d201731ef99cee6c75
mantezana <maximo00antezana@gmail.com>
2021-10-23 15:52:11 -0700

    DiAnne work

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
       path('', views.home, name='home'),
       path('about/', views.about, name='about'),
       path('dates/', views.dates_index, name='index'),
+      path('list/', views.dates_list, name='list'),
+      path('accounts/signup/', views.signup, name='signup'),
 ]
