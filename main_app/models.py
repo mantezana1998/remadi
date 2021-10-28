02e6eab63fb494353151515822a8d6d829c94199
mantezana <maximo00antezana@gmail.com>
2021-10-28 16:17:43 -0700

    joel rox

--- main_app/models.py
+++ main_app/models.py
@@ -5,13 +5,12 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Date(models.Model):
-    name = models.CharField(max_length=500)
-    location = models.CharField(max_length=100)
-    time = models.TimeField('Time of Event')
-    dates = models.DateField('Day of Event')
-    price = models.DecimalField(max_digits=500, decimal_places=2)
-    image = models.ImageField()
-    user = models.ManyToManyField(User)
+    name = models.CharField(max_length=500, null=True)
+    location = models.CharField(max_length=100, null=True)
+    time = models.TimeField('Time of Event', null=True)
+    dates = models.DateField(null=True, default=None)
+    price = models.DecimalField(max_digits=500, decimal_places=2, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'date_id': self.id})
