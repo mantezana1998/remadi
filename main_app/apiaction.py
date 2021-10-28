2e1fc6dc975f4dbaedc4171826c329b2a6786a83
Reggie <reginaldallonce@gmail.com>
2021-10-28 19:45:05 -0400

    idk

--- main_app/apiaction.py
+++ main_app/apiaction.py
@@ -7,15 +7,15 @@ def ticket_master_events():
     try:
         def grab_what_we_need(event):
             return {
-            # 'ticket_master_id': event['id*'],
-            # 'name': event['name'],
-            # 'location': event['_embedded']['venues'][0]['name'],
+            'ticket_master_id': event['id'],
+            'name': event['name'],
+            'location': event['_embedded']['venues'][0]['name'],
             'time': event['dates']['start']['localTime'],
             'dates': event['dates']['start']['localDate'],
             # 'price': float(event['priceRanges']),        
             }
         apikey = os.getenv("TICKET_MASTER")
-        response = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey={apikey}')
+        response = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey={apikey}')
         event = response.json()
         # print(event,'<------------------------------------------------------------Events')
         if event == None:
