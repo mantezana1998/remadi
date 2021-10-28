54e2686710561530e8a76b1a49079e8af350cc7c
mantezana <maximo00antezana@gmail.com>
2021-10-28 14:57:32 -0700

    dis crazy

--- main_app/apiaction.py
+++ main_app/apiaction.py
@@ -2,20 +2,33 @@ import requests, os
 from dotenv import load_dotenv
 import pprint
 load_dotenv()
+apikey = os.getenv("TICKET_MASTER")
+def single_event(event_id):
+    response = requests.get(f'https://app.ticketmaster.com/discovery/v2/events/{event_id}?apikey={apikey}')
+    json = response.json()
+    def grab_what_we_need(event):
+            return {
+            'ticket_master_id': event['id'],
+            'name': event['name'],
+            'location': event['_embedded']['venues'][0]['name'],
+            'time': event['dates']['start']['localTime'],
+            'dates': event['dates']['start']['localDate'],
+            # 'price': event['priceRanges'],        
+            }
+    return grab_what_we_need(json)
 
 def ticket_master_events():
     try:
         def grab_what_we_need(event):
             return {
-            # 'ticket_master_id': event['id*'],
-            # 'name': event['name'],
-            # 'location': event['_embedded']['venues'][0]['name'],
+            'ticket_master_id': event['id'],
+            'name': event['name'],
+            'location': event['_embedded']['venues'][0]['name'],
             'time': event['dates']['start']['localTime'],
-            # 'dates': event['dates']['start']['localDate'],
+            'dates': event['dates']['start']['localDate'],
             # 'price': event['priceRanges'],        
             }
-        apikey = os.getenv("TICKET_MASTER")
-        response = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey={apikey}')
+        response = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey={apikey}')
         event = response.json()
         # print(event,'<------------------------------------------------------------Events')
         if event == None:
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.db.models.fields import DateField, FloatField, TimeField, CharField
+from django.db.models.fields.files import ImageField
 from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Date(models.Model):
+    name = models.CharField(max_length=500)
+    location = models.CharField(max_length=100)
+    time = models.TimeField('Time of Event')
+    dates = models.DateField('Day of Event')
+    price = models.DecimalField(max_digits=500, decimal_places=2)
+    image = models.ImageField()
     user = models.ManyToManyField(User)
     
     def get_absolute_url(self):
